b1a7196086f9d12525fd088cc164fd9867467cbb
Locke <benkim96@gmail.com>
2023-03-19 19:38:51 -0700

    chore: refactor for utility functions for local storage

--- src/screens/Onboard.js
+++ src/screens/Onboard.js
@@ -4,84 +4,57 @@ import { Button, Input } from "react-native-elements";
 import "react-native-get-random-values";
 import "@ethersproject/shims";
 import { ethers } from "ethers";
-import * as SecureStore from "expo-secure-store";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchUsers, selectAllUsers } from "../store/users";
+import {
+  saveWalletData,
+  getWalletData,
+  deleteWalletData,
+} from "../utils/local-storage";
 
-async function save(key, value) {
-  console.log("saving: ", key, value);
-  await SecureStore.setItemAsync(key, value);
-}
-
-async function getValueFor(key) {
-  let result = await SecureStore.getItemAsync(key);
-  if (result) {
-    console.log("fetching: ", key, result);
-    return result.toString();
-  } else {
-    return "";
-  }
-}
-
-async function deleteValueFor(key) {
-  let result = await SecureStore.deleteItemAsync(key);
-  console.log("deleting: ", key);
-  if (result) {
-    console.log("deleting: ", key);
-    return result.toString();
-  } else {
-    return "";
-  }
-}
-
-export default function OnboardScreen({ navigation }) {
-  const [toAddress, setToAddress] = useState("");
-  const [value, setValue] = useState("");
-  const [signedTransaction, setSignedTransaction] = useState("");
-
-  const [address, setAddress] = useState("");
-  const [privateKey, setPrivateKey] = useState("");
-  const [mnemonic, setMnemonic] = useState("");
-
+export default function OnboardScreen() {
+  // const dispatch = useDispatch();
+  // const { loading } = useSelector((state) => state.users);
+  // const users = useSelector(selectAllUsers);
+  const [walletData, setWalletData] = useState({
+    address: "",
+    privateKey: "",
+    mnemonic: "",
+  });
   const [seedPhrase, setSeedPhrase] = useState("");
+
+  // useEffect(() => {
+  //   dispatch(fetchUsers());
+  // }, []);
 
   useEffect(() => {
     async function fetchKeys() {
-      setAddress(await getValueFor("oak-app-address"));
-      setPrivateKey(await getValueFor("oak-app-private-key"));
-      setMnemonic(await getValueFor("oak-app-mnemonic"));
+      setWalletData(await getWalletData());
     }
     fetchKeys();
   }, []);
 
   const deleteWallet = async () => {
-    await deleteValueFor("oak-app-address");
-    await deleteValueFor("oak-app-private-key");
-    await deleteValueFor("oak-app-mnemonic");
-
-    setAddress("");
-    setPrivateKey("");
-    setMnemonic("");
+    setWalletData(await deleteWalletData());
   };
 
   const configureWallet = async (useSeed) => {
     try {
       var wallet;
       if (useSeed) {
-        console.log("importing wallet");
         wallet = ethers.Wallet.fromMnemonic(seedPhrase);
       } else {
-        console.log("creating wallet");
         wallet = ethers.Wallet.createRandom();
       }
       // store wallet information in encrypted keychain
       const generatedAddress = await wallet.getAddress();
-      await save("oak-app-address", generatedAddress);
-      await save("oak-app-private-key", wallet.privateKey);
-      await save("oak-app-mnemonic", wallet.mnemonic.phrase);
-
-      // store wallet information in local state
-      setAddress(await getValueFor("oak-app-address"));
-      setPrivateKey(await getValueFor("oak-app-private-key"));
-      setMnemonic(await getValueFor("oak-app-mnemonic"));
+      setWalletData(
+        await saveWalletData(
+          generatedAddress,
+          wallet.privateKey,
+          wallet.mnemonic.phrase
+        )
+      );
 
       if (useSeed) {
         console.log("successful wallet import");
@@ -93,48 +66,32 @@ export default function OnboardScreen({ navigation }) {
     }
   };
 
-  const signTransaction = async () => {
-    try {
-      const wallet = new ethers.Wallet(privateKey);
-      const nonce = await wallet.getTransactionCount();
-      const gasPrice = await wallet.provider.getGasPrice();
-      const transaction = {
-        to: toAddress,
-        value: ethers.utils.parseEther(value),
-        nonce,
-        gasPrice,
-        gasLimit: 21000,
-      };
-      const signedTx = await wallet.signTransaction(transaction);
-      setSignedTransaction(signedTx);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
     <View style={styles.container}>
       <Text>OAK CURRENCY</Text>
-      <Button title="Create Wallet" onPress={() => configureWallet(false)} />
-      {address !== "" && privateKey !== "" ? (
-        <View>
-          <Text>Address: {address}</Text>
-          <Text>Mnemonic: {mnemonic}</Text>
-        </View>
-      ) : null}
-      <Input
-        label="Seed Phrase"
-        placeholder="Input your Seed Phrase"
-        value={seedPhrase}
-        onChangeText={setSeedPhrase}
-        autoCapitalize="none"
-        secureTextEntry
-      />
-      <Button title="Import Wallet" onPress={() => configureWallet(true)} />
-      <Button title="Delete Wallet" onPress={deleteWallet} />
-      {/* <StatusBar style="auto" /> */}
-      <Button title="Onboard" onPress={() => navigation.navigate("Onboard")} />
-      <Button title="Wallet" onPress={() => navigation.navigate("Wallet")} />
+
+      {walletData.address !== "" && walletData.privateKey !== "" ? (
+        <>
+          <Text>Address: {walletData.address}</Text>
+          <Button title="Delete Wallet" onPress={deleteWallet} />
+        </>
+      ) : (
+        <>
+          <Button
+            title="Create Wallet"
+            onPress={() => configureWallet(false)}
+          />
+          <Input
+            label="Seed Phrase"
+            placeholder="Input your Seed Phrase"
+            value={seedPhrase}
+            onChangeText={setSeedPhrase}
+            autoCapitalize="none"
+            secureTextEntry
+          />
+          <Button title="Import Wallet" onPress={() => configureWallet(true)} />
+        </>
+      )}
     </View>
   );
 }
---
+++ src/utils/local-storage.js
@@ -0,0 +1,68 @@
+import * as SecureStore from "expo-secure-store";
+
+export const ADDRESS = "oak-app-address";
+export const PRIVATE_KEY = "oak-app-private-key";
+export const MNEMONIC = "oak-app-mnemonic";
+
+// SECURE STORE FUNCTIONS
+export async function save(key, value) {
+  await SecureStore.setItemAsync(key, value);
+}
+
+export async function getValueFor(key) {
+  let result = await SecureStore.getItemAsync(key);
+  if (result) {
+    return result.toString();
+  } else {
+    return "";
+  }
+}
+
+export async function deleteValueFor(key) {
+  let result = await SecureStore.deleteItemAsync(key);
+  if (result) {
+    return result.toString();
+  } else {
+    return "";
+  }
+}
+
+// WALLET STORE UTILITY FUNCTIONS
+export async function saveWalletData(address, privateKey, mnemonic) {
+  await save(ADDRESS, address);
+  await save(PRIVATE_KEY, privateKey);
+  await save(MNEMONIC, mnemonic);
+  const payload = {
+    address: address,
+    privateKey: privateKey,
+    mnemonic: mnemonic,
+  };
+  console.log("WALLET DATA: ", payload);
+  return payload;
+}
+
+export async function getWalletData() {
+  const address = await getValueFor(ADDRESS);
+  const privateKey = await getValueFor(PRIVATE_KEY);
+  const mnemonic = await getValueFor(MNEMONIC);
+  const payload = {
+    address: address,
+    privateKey: privateKey,
+    mnemonic: mnemonic,
+  };
+  console.log("WALLET DATA: ", payload);
+  return payload;
+}
+
+export async function deleteWalletData() {
+  const address = await deleteValueFor(ADDRESS);
+  const privateKey = await deleteValueFor(PRIVATE_KEY);
+  const mnemonic = await deleteValueFor(MNEMONIC);
+  const payload = {
+    address: address,
+    privateKey: privateKey,
+    mnemonic: mnemonic,
+  };
+  console.log("WALLET DATA: ", payload);
+  return payload;
+}
