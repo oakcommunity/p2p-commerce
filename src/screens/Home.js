21e1d4ffa21b046dd09cabc0cfae4abea8b3752b
Locke <benkim96@gmail.com>
2023-03-22 15:02:09 -0700

    gpt checkpoint for tx send

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,6 +2,26 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 export default function HomeScreen() {
+  const sendUSDCTransaction = async (wallet, recipientAddress, amount) => {
+    const provider = ethers.getDefaultProvider("polygon");
+    const connectedWallet = wallet.connect(provider);
+
+    const usdcContract = new ethers.Contract(
+      USDC_CONTRACT_ADDRESS,
+      USDC_ABI,
+      connectedWallet
+    );
+    const amountInWei = ethers.utils.parseUnits(amount.toString(), 6);
+
+    const transactionResponse = await usdcContract.transfer(
+      recipientAddress,
+      amountInWei
+    );
+    const transactionReceipt = await transactionResponse.wait();
+
+    return transactionReceipt;
+  };
+
   return (
     <View style={styles.container}>
       <Text>OAK CURRENCY</Text>
